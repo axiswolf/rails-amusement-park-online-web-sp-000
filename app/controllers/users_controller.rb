fb6aecec8e152f6a0fd56f1b7eddfd50fce4cdd1
Karen V <karenvo@usc.edu>
2020-02-13 07:48:20 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
     def index
         @users = User.all
     end
@@ -14,7 +13,7 @@ class UsersController < ApplicationController
         session[:user_id] = @user.id
         redirect_to user_path(@user)
     end
-
+    
     def show
         if session[:user_id]
             @user = User.find(session[:user_id])
@@ -27,11 +26,10 @@ class UsersController < ApplicationController
     def welcome
 
     end
-  private
 
-  def user_params
-    params.require(:user).permit(:name, :password, :happiness, :nausea, :tickets, :height)
-  end
+    private
 
-
+    def user_params
+      params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :password, :admin)
+    end
 end
