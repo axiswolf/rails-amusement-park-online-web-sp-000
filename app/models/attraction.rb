c06d48c16aa6cad99e2130a92a984501b21ebf47
Karen V <karenvo@usc.edu>
2020-02-13 07:43:18 +0000

    Automatically backed up by Learn

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,8 +1,6 @@
 class Attraction < ApplicationRecord
-<<<<<<< HEAD
+
     has_many :rides
     has_many :users, through: :rides
-=======
-  has_many :rides
->>>>>>> 4293e396b5f534918e97fa9e133fb0fb60e6578e
+    has_many :rides
 end
